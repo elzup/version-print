b615c35b73a95767fbf22636f8329c558bc6326a
elzup <guild0105@gmail.com>
2018-09-21 16:40:07 +0900

    module test

--- src/test.js
+++ src/test.js
@@ -1,6 +1,44 @@
 // @flow
-const m = require('.')
 
-test('snapshot', () => {
-  expect(m('unicorn')).toMatchSnapshot()
+import inquirer from 'inquirer'
+import cp from 'child_process'
+import m from '.'
+
+jest.mock('inquirer')
+jest.mock('child_process')
+
+const spyLog = jest.spyOn(console, 'log')
+spyLog.mockImplementation(x => x)
+
+cp.execSync = jest.fn().mockReturnValue(`stdout`)
+
+test('snapshot', async () => {
+  inquirer.prompt = jest
+    .fn()
+    .mockResolvedValue({ items: ['OS', 'GoogleChrome'] })
+  await m()
+  expect(spyLog.mock.calls).toMatchInlineSnapshot(`
+Array [
+  Array [
+    "OS",
+  ],
+  Array [
+    "[90m$ system_profiler SPSoftwareDataType |grep Version |sed \\"s/^ *//\\"[39m",
+  ],
+  Array [
+    "stdout",
+  ],
+  Array [
+    "GoogleChrome",
+  ],
+  Array [
+    "[90m$ /Applications/Google\\\\ Chrome.app/Contents/MacOS/Google\\\\ Chrome --version[39m",
+  ],
+  Array [
+    "stdout",
+  ],
+]
+`)
+
+  // expect(m('unicorn')).toMatchSnapshot()
 })
